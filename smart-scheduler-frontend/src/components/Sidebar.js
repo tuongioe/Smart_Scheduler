f310d838d6e23c481411b6aafc5dc24889f84481
duythai03 <21522578@gm.uit.edu.vn>
2024-05-14 19:38:18 +0700

    update: add active effect for sidebar

--- smart-scheduler-frontend/src/components/Sidebar.js
+++ smart-scheduler-frontend/src/components/Sidebar.js
@@ -1,21 +1,45 @@
 import React from "react";
 import { FaCalendar, FaWandMagicSparkles, FaGear } from "react-icons/fa6";
+import { useLocation } from "react-router-dom";
 
 export default function Sidebar() {
+  const location = useLocation();
+
   return (
     <aside className="flex flex-col justify-center items-center w-10 h-full">
-      <div className="flex flex-col justify-center items-center h-full text-[#929292] text-4xl">
-        <a href="/" className="mb-6">
+      <div className="flex flex-col justify-center items-center h-full text-[#929292] text-3xl">
+        <a
+          href="/"
+          className={`mb-6 p-3 rounded-xl ${
+            location.pathname === "/"
+              ? "activeLink bg-[#232E30] text-[#00717F]"
+              : ""
+          }`}
+        >
           <i>
             <FaCalendar />
           </i>
         </a>
-        <a href="/ai-scheduler" className="mb-6">
+        <a
+          href="/ai-scheduler"
+          className={`mb-6 p-3 rounded-xl ${
+            location.pathname === "/ai-scheduler"
+              ? "activeLink bg-[#232E30] text-[#00717F]"
+              : ""
+          }`}
+        >
           <i>
             <FaWandMagicSparkles />
           </i>
         </a>
-        <a href="/setting" className="mb-6">
+        <a
+          href="/setting"
+          className={`mb-6 p-3 rounded-xl ${
+            location.pathname === "/setting"
+              ? "activeLink bg-[#232E30] text-[#00717F]"
+              : ""
+          }`}
+        >
           <i>
             <FaGear />
           </i>
--- smart-scheduler-frontend/src/route/AIScheduler.js
+++ smart-scheduler-frontend/src/route/AIScheduler.js
@@ -2,8 +2,42 @@ import React from "react";
 
 export default function AIScheduler() {
   return (
-    <div className="text-center">
-      <h2 className="text-2xl">AI Scheduler</h2>
+    <div>
+      <div>
+        <input
+          type="text"
+          name="taskName"
+          id="taskName"
+          placeholder="Enter title here"
+        />
+      </div>
+      <div>
+        <label for="estimateTime">Estimate Time</label>
+        <select name="estimateTime" id="estimateTime">
+          <option value="30" selected>
+            30 min
+          </option>
+          <option value="60">1 hour</option>
+          <option value="120">2 hours</option>
+          <option value="180">3 hours</option>
+          <option value="300">5 hours</option>
+          <option value="720">12 hours</option>
+          <option value="1440">1 day</option>
+        </select>
+      </div>
+      <div>
+        <label for="calendarType">Calendar</label>
+        <select name="calendarType" id="calendarType">
+          <option value="health" selected>
+            Health
+          </option>
+          <option value="personal">Personal</option>
+          <option value="work">Work</option>
+        </select>
+      </div>
+      <div></div>
+      <div></div>
+      <div></div>
     </div>
   );
 }
