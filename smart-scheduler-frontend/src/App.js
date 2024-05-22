19d8e9fbbad9f8ce68c94b617a364db451760f61
duythai03 <21522578@gm.uit.edu.vn>
2024-05-22 14:43:35 +0700

    fix: delete sidebar in login page

--- smart-scheduler-frontend/src/App.js
+++ smart-scheduler-frontend/src/App.js
@@ -2,9 +2,6 @@ import "./App.css";
 import React from "react";
 import { Routes, Route } from "react-router-dom";
 import Sidebar from "./components/Sidebar";
-import CalendarPage from "./route/Calendar.js";
-import AISchedulerPage from "./route/AIScheduler.js";
-import SettingApp from "./route/SettingApp.js";
 import RegisterPage from "./RegisterPage.js";
 import LoginPage from "./LoginPage.js";
 function App() {
@@ -12,16 +9,10 @@ function App() {
     <React.Fragment>
       <div className="container mx-auto md:max-w-[1024px]">
         <div className="flex flex-col md:flex-row md:h-screen">
-          <div className="w-full md:w-2/12 min-w-[100px]">
-            <Sidebar />
-          </div>
           <div className="w-full md:w-10/12">
             <Routes>
-              <Route path="/" element={<CalendarPage />} />
-              <Route path="/ai-scheduler" element={<AISchedulerPage />} />
-              <Route path="/setting" element={<SettingApp />} />
               <Route path="/register" element={<RegisterPage />} />
-              <Route path="/login" element={<LoginPage />} />
+              <Route path="/" element={<LoginPage />} />
             </Routes>
           </div>
         </div>
