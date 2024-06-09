0de35a8aa710890c2b009a205a3fb6237ea5eba4
duythai03 <21522578@gm.uit.edu.vn>
2024-06-10 02:31:35 +0700

    fix: update generate scheduler

--- smart-scheduler-frontend/src/App.js
+++ smart-scheduler-frontend/src/App.js
@@ -1,21 +1,99 @@
-import './assets/App.css';
-import React from 'react';
-import { Routes, Route } from 'react-router-dom';
-import Sidebar from './components/Sidebar';
-import { AIScheduler, Calendar, LoginPage, RegisterPage } from './pages';
+import "./assets/App.css";
+import React, { useState } from "react";
+import { Routes, Route, Navigate } from "react-router-dom";
+import Sidebar from "./components/Sidebar";
+import { AIScheduler, Calendar, LoginPage, RegisterPage } from "./pages";
 
 const App = () => {
+  const [isAuthenticated, setIsAuthenticated] = useState(false);
+
+  const handleLogin = (username, password) => {
+    if (username === "admin" && password === "admin") {
+      setIsAuthenticated(true);
+    }
+  };
+
   return (
     <React.Fragment>
-      <div className="container mx-auto md:max-w-[1024px]">
-        <div className="flex flex-col md:flex-row md:h-screen">
-          <div className="w-full md:w-10/12">
-            <Routes>
-              <Route element={<LoginPage />} index="true" />
-              <Route path="/register" element={<RegisterPage />} />
-              <Route path="/calendar" element={<Calendar />} />
-              <Route path="/ai" element={<AIScheduler />} />
-            </Routes>
+      <div className="w-screen h-screen relative">
+        <svg
+          style={{ position: "absolute", top: 0, left: 0 }}
+          width="236"
+          height="227"
+          viewBox="0 0 236 227"
+          fill="none"
+          xmlns="http://www.w3.org/2000/svg"
+        >
+          <g filter="url(#filter0_f_17_104)">
+            <path
+              d="M76 16C76 44.1665 49.5848 67 17 67C-15.5848 67 -42 44.1665 -42 16C-42 -12.1665 -15.5848 -35 17 -35C49.5848 -35 76 -12.1665 76 16Z"
+              fill="#00A9BF"
+              fill-opacity="0.8"
+            />
+          </g>
+          <defs>
+            <filter
+              id="filter0_f_17_104"
+              x="-202"
+              y="-195"
+              width="438"
+              height="422"
+              filterUnits="userSpaceOnUse"
+              color-interpolation-filters="sRGB"
+            >
+              <feFlood flood-opacity="0" result="BackgroundImageFix" />
+              <feBlend
+                mode="normal"
+                in="SourceGraphic"
+                in2="BackgroundImageFix"
+                result="shape"
+              />
+              <feGaussianBlur
+                stdDeviation="80"
+                result="effect1_foregroundBlur_17_104"
+              />
+            </filter>
+          </defs>
+        </svg>
+
+        <div className="container mx-auto md:max-w-[1024px]">
+          <div className="flex flex-col md:flex-row md:h-screen">
+            {/* Render Sidebar if authenticated */}
+            {isAuthenticated && (
+              <div className="w-full md:w-2/12">
+                <Sidebar />
+              </div>
+            )}
+
+            <div
+              className={
+                isAuthenticated ? "w-full md:w-10/12" : "w-full relative"
+              }
+            >
+              <Routes>
+                <Route
+                  path="/"
+                  element={
+                    isAuthenticated ? (
+                      <Navigate to="/ai" />
+                    ) : (
+                      <LoginPage onLogin={handleLogin} />
+                    )
+                  }
+                />
+                <Route path="/register" element={<RegisterPage />} />
+                <Route
+                  path="/calendar"
+                  element={isAuthenticated ? <Calendar /> : <Navigate to="/" />}
+                />
+                <Route
+                  path="/ai"
+                  element={
+                    isAuthenticated ? <AIScheduler /> : <Navigate to="/" />
+                  }
+                />
+              </Routes>
+            </div>
           </div>
         </div>
       </div>
--- smart-scheduler-frontend/src/pages/LoginPage.js
+++ smart-scheduler-frontend/src/pages/LoginPage.js
@@ -1,76 +1,92 @@
-import '../assets/User.css';
-import { FiEye, FiEyeOff } from 'react-icons/fi';
-import React, { useState } from 'react';
-import { Link } from 'react-router-dom';
+import "../assets/User.css";
+import { FiEye, FiEyeOff } from "react-icons/fi";
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
 
-function LoginPage() {
+const LoginPage = ({ onLogin }) => {
   const [showPassword, setShowPassword] = useState(false);
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
   const togglePasswordVisibility = () => {
     setShowPassword((prevShowPassword) => !prevShowPassword);
   };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    onLogin(email, password);
+  };
+
   return (
     <div
       className="Login__Block"
-      style={{ backgroundImage: 'linear-gradient(to right, #146D78, #1F3336)' }}
+      style={{ backgroundImage: "linear-gradient(to right, #146D78, #1F3336)" }}
     >
       <h1>Login</h1>
-      <div className="Login__Input">
-        <label htmlFor="email">E-mail</label>
-        <input
-          type="text"
-          id="email"
-          placeholder="Enter your E-mail"
-          style={{
-            backgroundImage: 'linear-gradient(to right, #59898F, #2F4244)',
-          }}
-        ></input>
-      </div>
-      <div className="Login__Input">
-        <label htmlFor="password">Password</label>
-        <input
-          type={showPassword ? 'text' : 'password'}
-          id="password"
-          placeholder="Enter your password"
-          style={{
-            backgroundImage: 'linear-gradient(to right, #59898F, #2F4244)',
-          }}
-        ></input>
-        {showPassword ? (
-          <FiEye
-            size={24}
-            className="Login__Icon1"
-            onClick={togglePasswordVisibility}
+      <form onSubmit={handleSubmit}>
+        <div className="Login__Input">
+          <label htmlFor="email">E-mail</label>
+          <input
+            type="text"
+            id="email"
+            placeholder="Enter your E-mail"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+            style={{
+              backgroundImage: "linear-gradient(to right, #59898F, #2F4244)",
+            }}
           />
-        ) : (
-          <FiEyeOff
-            size={24}
-            className="Login__Icon1"
-            onClick={togglePasswordVisibility}
+        </div>
+        <div className="Login__Input">
+          <label htmlFor="password">Password</label>
+          <input
+            type={showPassword ? "text" : "password"}
+            id="password"
+            placeholder="Enter your password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+            style={{
+              backgroundImage: "linear-gradient(to right, #59898F, #2F4244)",
+            }}
           />
-        )}
-      </div>
-      <p className="Forgot">Forgot password ?</p>
-      <button
-        className="Login__Button"
-        style={{
-          backgroundImage: 'linear-gradient(to left, #00717F, #00777F)',
-        }}
-      >
-        Login
-      </button>
+          {showPassword ? (
+            <FiEye
+              size={24}
+              className="Login__Icon1"
+              onClick={togglePasswordVisibility}
+            />
+          ) : (
+            <FiEyeOff
+              size={24}
+              className="Login__Icon1"
+              onClick={togglePasswordVisibility}
+            />
+          )}
+        </div>
+        <p className="Forgot">Forgot password ?</p>
+        <button
+          type="submit"
+          className="Login__Button"
+          style={{
+            backgroundImage: "linear-gradient(to left, #00717F, #00777F)",
+          }}
+        >
+          Login
+        </button>
+      </form>
       <p className="Login__Text">
         No account ?
         <Link
           to="/register"
-          style={{ fontWeight: 'bold', color: 'white', textDecoration: 'none' }}
+          style={{ fontWeight: "bold", color: "white", textDecoration: "none" }}
         >
-          {' '}
-          Register{' '}
+          {" "}
+          Register{" "}
         </Link>
         here
       </p>
     </div>
   );
-}
+};
 
 export default LoginPage;
